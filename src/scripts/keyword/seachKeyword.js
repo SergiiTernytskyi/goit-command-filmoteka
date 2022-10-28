9099ea6a5aa5556f16b9f2622ab7aa85851be84c
Serhii-Ter <serhij86@gmail.com>
2022-10-28 21:54:46 +0300

    fix

--- src/scripts/keyword/seachKeyword.js
+++ src/scripts/keyword/seachKeyword.js
@@ -1,53 +1,47 @@
-
-import { MovieApi } from './MovieApi'
+import { MovieApi } from './MovieApi';
 import listOfGenres from '../../js/genresList';
 import { clearGallery } from './clearGallery';
 import Notiflix from 'notiflix';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { refs } from '../refs';
 
-
 let searchQuery = '';
 
 const movies = new MovieApi();
 
 refs.form.addEventListener('submit', onSearch);
- 
+
 function onSearch(e) {
   e.preventDefault();
- 
-searchQuery = e.currentTarget.elements.query.value.trim().toLowerCase();
-    if (!searchQuery) {
-      return Notiflix.Notify.warning( `Please enter name of the movie`);
+
+  searchQuery = e.currentTarget.elements.query.value.trim().toLowerCase();
+  if (!searchQuery) {
+    return Notiflix.Notify.warning(`Please enter name of the movie`);
   }
-  
+
   movies.getMovie(searchQuery).then(({ results }) => {
-    
     if (results.length === 0) {
-       Report.failure(
-         'No Result &#128584',
-         'Search result not successful. Enter the correct movie name and ',
-         'Okay &#128527'
-       );
-      
+      Report.failure(
+        'No Result &#128584',
+        'Search result not successful. Enter the correct movie name and ',
+        'Okay &#128527'
+      );
     }
     refs.form.reset();
     clearGallery();
     const markup = createMarkup(results);
     refs.list.insertAdjacentHTML('beforeend', markup);
-    
   });
-    
-};
+}
 
 /* -------------------------------------------------------------------------- */
 /*                 Видалити коли буде окремо винесена функція                 */
 /* -------------------------------------------------------------------------- */
 
- function createMarkup(films) {
-   return films
-     .map(({ poster_path, original_title, genre_ids, release_date }) => {
-       return `<li class="card__item">
+function createMarkup(films) {
+  return films
+    .map(({ poster_path, original_title, genre_ids, release_date }) => {
+      return `<li class="card__item">
         <div class="card__thumb">
             <img class="card__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}">
         </div>
@@ -60,8 +54,6 @@ searchQuery = e.currentTarget.elements.query.value.trim().toLowerCase();
             )}</span> | <span class="card__date">${release_date}</span>
         </p>
         </li>`;
-     })
-     .join('');
-   
-
- }
+    })
+    .join('');
+}
