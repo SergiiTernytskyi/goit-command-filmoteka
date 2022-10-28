7ac7b58f5781be122962ede26ca978d41cfb11de
Vitalii Tymkiv <vitaliy.tymkiv@gmail.com>
2022-10-28 13:02:39 +0300

    add localstorage

---
+++ src/library.js
@@ -0,0 +1,8 @@
+import { initModal } from './scripts/filmmodal';
+import { loadAllFilms } from './scripts/mylibrary';
+import { loadWatchedFilms } from './scripts/mylibrary';
+import { loadQueuedFilms } from './scripts/mylibrary';
+loadAllFilms();
+loadWatchedFilms();
+loadQueuedFilms();
+initModal(873125);
