7465a75c5ad96da711308cf256bd1fc61bc1d984
Juliia Kapuza <kapuzajuliia@gmail.com>
2022-10-29 15:12:52 +0300

    add image

--- src/scripts/filmmodal.js
+++ src/scripts/filmmodal.js
@@ -1,43 +1,43 @@
-import {
-  toggleWatched,
-  toggleQueue,
-  isFilmWatched,
-  isFilmQueued,
-} from './mylibrary';
-import { refs } from './refs';
+// import {
+//   toggleWatched,
+//   toggleQueue,
+//   isFilmWatched,
+//   isFilmQueued,
+// } from './mylibrary';
+// import { refs } from './refs';
 
-function updateWatchedBtnText(filmId) {
-  if (isFilmWatched(filmId)) {
-    refs.watchedBtn.textContent = 'Remove from watched';
-    refs.watchedBtn.classList.add('active');
-  } else {
-    refs.watchedBtn.textContent = 'Add to watched';
-    refs.watchedBtn.classList.remove('active');
-  }
-}
-function updateQueuedBtnText(filmId) {
-  if (isFilmQueued(filmId)) {
-    refs.queueBtn.textContent = 'Remove from queued';
-    refs.queueBtn.classList.add('active');
-  } else {
-    refs.queueBtn.textContent = 'Add to queued';
-    refs.queueBtn.classList.remove('active');
-  }
-}
+// function updateWatchedBtnText(filmId) {
+//   if (isFilmWatched(filmId)) {
+//     refs.watchedBtn.textContent = 'Remove from watched';
+//     refs.watchedBtn.classList.add('active');
+//   } else {
+//     refs.watchedBtn.textContent = 'Add to watched';
+//     refs.watchedBtn.classList.remove('active');
+//   }
+// }
+// function updateQueuedBtnText(filmId) {
+//   if (isFilmQueued(filmId)) {
+//     refs.queueBtn.textContent = 'Remove from queued';
+//     refs.queueBtn.classList.add('active');
+//   } else {
+//     refs.queueBtn.textContent = 'Add to queued';
+//     refs.queueBtn.classList.remove('active');
+//   }
+// }
 
-export function setupModalButtons(filmId) {
-  console.log(document.querySelector('.film-modal__watch-btn'));
-  refs.watchedBtn.addEventListener('click', () => {
-    toggleWatched(filmId);
-    updateWatchedBtnText(filmId);
-  });
-  refs.queueBtn.addEventListener('click', () => {
-    toggleQueue(filmId);
-    updateQueuedBtnText(filmId);
-  });
-  updateWatchedBtnText(filmId);
-  updateQueuedBtnText(filmId);
-}
+// export function setupModalButtons(filmId) {
+//   console.log(document.querySelector('.film-modal__watch-btn'));
+//   refs.watchedBtn.addEventListener('click', () => {
+//     toggleWatched(filmId);
+//     updateWatchedBtnText(filmId);
+//   });
+//   refs.queueBtn.addEventListener('click', () => {
+//     toggleQueue(filmId);
+//     updateQueuedBtnText(filmId);
+//   });
+//   updateWatchedBtnText(filmId);
+//   updateQueuedBtnText(filmId);
+// }
 // console.log(refs.watchedBtn);
 // console.log(refs.queueBtn);
 // console.log(refs.myLibraryFilmList);
--- src/scripts/mylibrary.js
+++ src/scripts/mylibrary.js
@@ -1,97 +1,97 @@
-import { getFilmbyId } from './fetchfromAPI';
-import { initModal } from './filmmodal';
-import { save, load, remove } from './localestorageservices';
-import { refs } from './refs';
+// import { getFilmbyId } from './fetchfromAPI';
+// import { initModal } from './filmmodal';
+// import { save, load, remove } from './localestorageservices';
+// import { refs } from './refs';
 
-function toggleItems(filmId, storageKey) {
-  let watchedItems = load(storageKey);
+// function toggleItems(filmId, storageKey) {
+//   let watchedItems = load(storageKey);
   // watchedItems = watchedItems ? watchedItems : [];
-  if (Array.isArray(watchedItems)) {
-    const existIndex = watchedItems.indexOf(filmId);
-    if (existIndex > -1) {
-      watchedItems.splice(existIndex, 1);
-    } else {
-      watchedItems.push(filmId);
-    }
-  } else {
-    watchedItems = [filmId];
-  }
-  save(storageKey, watchedItems);
-}
-export function toggleWatched(filmId) {
-  toggleItems(filmId, 'watched');
-}
+//   if (Array.isArray(watchedItems)) {
+//     const existIndex = watchedItems.indexOf(filmId);
+//     if (existIndex > -1) {
+//       watchedItems.splice(existIndex, 1);
+//     } else {
+//       watchedItems.push(filmId);
+//     }
+//   } else {
+//     watchedItems = [filmId];
+//   }
+//   save(storageKey, watchedItems);
+// }
+// export function toggleWatched(filmId) {
+//   toggleItems(filmId, 'watched');
+// }
 
-export function toggleQueue(filmId) {
-  toggleItems(filmId, 'queue');
-}
+// export function toggleQueue(filmId) {
+//   toggleItems(filmId, 'queue');
+// }
 
-function findFilmIndex(filmId, storageKey) {
-  let items = load(storageKey);
-  if (Array.isArray(items)) {
-    return items.indexOf(filmId);
-  }
-  return -1;
-}
+// function findFilmIndex(filmId, storageKey) {
+//   let items = load(storageKey);
+//   if (Array.isArray(items)) {
+//     return items.indexOf(filmId);
+//   }
+//   return -1;
+// }
 
-export function isFilmWatched(filmId) {
-  return findFilmIndex(filmId, 'watched') > -1;
-}
+// export function isFilmWatched(filmId) {
+//   return findFilmIndex(filmId, 'watched') > -1;
+// }
 
-export function isFilmQueued(filmId) {
-  return findFilmIndex(filmId, 'queue') > -1;
-}
+// export function isFilmQueued(filmId) {
+//   return findFilmIndex(filmId, 'queue') > -1;
+// }
 
-export async function loadAllFilms() {
-  const watchedFilms = load('watched') || [];
-  const queuedFilms = load('queue') || [];
-  const filmsId = [...watchedFilms, ...queuedFilms];
-  console.log(filmsId);
-  const filmsPromises = filmsId.map(async filmId => await getFilmbyId(filmId));
-  const films = await Promise.all(filmsPromises);
-  refs.myLibraryFilmList.insertAdjacentHTML(
-    'beforeend',
-    createFilmMarkup(films)
-  );
-}
-export async function loadWatchedFilms() {
-  const watchedFilms = load('watched') || [];
+// export async function loadAllFilms() {
+//   const watchedFilms = load('watched') || [];
+//   const queuedFilms = load('queue') || [];
+//   const filmsId = [...watchedFilms, ...queuedFilms];
+//   console.log(filmsId);
+//   const filmsPromises = filmsId.map(async filmId => await getFilmbyId(filmId));
+//   const films = await Promise.all(filmsPromises);
+//   refs.myLibraryFilmList.insertAdjacentHTML(
+//     'beforeend',
+//     createFilmMarkup(films)
+//   );
+// }
+// export async function loadWatchedFilms() {
+//   const watchedFilms = load('watched') || [];
 
-  const watchedfilmsId = [...watchedFilms];
-  console.log(watchedfilmsId);
-  const filmsPromises = watchedfilmsId.map(
-    async filmId => await getFilmbyId(filmId)
-  );
-  const films = await Promise.all(filmsPromises);
-  refs.myLibraryFilmList.insertAdjacentHTML(
-    'beforeend',
-    createFilmMarkup(films)
-  );
-}
+//   const watchedfilmsId = [...watchedFilms];
+//   console.log(watchedfilmsId);
+//   const filmsPromises = watchedfilmsId.map(
+//     async filmId => await getFilmbyId(filmId)
+//   );
+//   const films = await Promise.all(filmsPromises);
+//   refs.myLibraryFilmList.insertAdjacentHTML(
+//     'beforeend',
+//     createFilmMarkup(films)
+//   );
+// }
 
-export async function loadQueuedFilms() {
-  const queuedFilms = load('queue') || [];
+// export async function loadQueuedFilms() {
+//   const queuedFilms = load('queue') || [];
 
-  const queuedfilmsId = [...queuedFilms];
-  console.log(queuedfilmsId);
-  const filmsPromises = queuedfilmsId.map(
-    async filmId => await getFilmbyId(filmId)
-  );
-  const films = await Promise.all(filmsPromises);
-  refs.myLibraryFilmList.insertAdjacentHTML(
-    'beforeend',
-    createFilmMarkup(films)
-  );
-}
+//   const queuedfilmsId = [...queuedFilms];
+//   console.log(queuedfilmsId);
+//   const filmsPromises = queuedfilmsId.map(
+//     async filmId => await getFilmbyId(filmId)
+//   );
+//   const films = await Promise.all(filmsPromises);
+//   refs.myLibraryFilmList.insertAdjacentHTML(
+//     'beforeend',
+//     createFilmMarkup(films)
+//   );
+// }
 
-function createFilmMarkup(films) {
-  return films
-    .map(({ title }) => {
-      return /*html*/ `
+// function createFilmMarkup(films) {
+//   return films
+//     .map(({ title }) => {
+//       return /*html*/ `
   
-    <li class="film-card">${title}</li>
+//     <li class="film-card">${title}</li>
    
-`;
-    })
-    .join('');
-}
+// `;
+//     })
+//     .join('');
+// }
