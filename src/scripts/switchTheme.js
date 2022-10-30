8600c82345ff59d40469766a5bb4ec25d5335580
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-31 00:01:41 +0200

    add script

--- src/scripts/switchTheme.js
+++ src/scripts/switchTheme.js
@@ -3,7 +3,31 @@ const refs = {
   lightBtn: document.querySelector('.them-light-btn'),
   lightIcon: document.querySelector('.icon-light'),
   darkIcon: document.querySelector('.icon-dark'),
+  main: document.querySelector('main'),
+  footer: document.querySelector('footer'),
+  footerText: document.querySelectorAll('.footer-content'),
+  galleryTitle: document.getElementsByClassName('card__title'),
 };
+
+function darkThemeStyles() {
+  refs.main.classList.add('main--dark');
+  refs.footer.classList.add('footer--dark');
+  refs.footerText.forEach(item => {
+    item.classList.add('footer-content--dark');
+  });
+
+  for (let title of refs.galleryTitle) {
+    title.classList.add('card__title--dark');
+  }
+}
+
+function lightThemeStyles() {
+  refs.main.classList.remove('main--dark');
+  refs.footer.classList.remove('footer--dark');
+  refs.footerText.forEach(item => {
+    item.classList.remove('footer-content--dark');
+  });
+}
 
 refs.darkBtn.addEventListener('click', onDarkBtnClick);
 
@@ -12,6 +36,8 @@ function onDarkBtnClick() {
   refs.darkBtn.classList.remove('hide-btn');
   refs.lightIcon.classList.add('opacity-icon');
   refs.darkIcon.classList.remove('opacity-icon');
+
+  darkThemeStyles();
 }
 
 refs.lightBtn.addEventListener('click', onLightBtnClick);
@@ -21,4 +47,6 @@ function onLightBtnClick() {
   refs.lightBtn.classList.remove('hide-btn');
   refs.lightIcon.classList.remove('opacity-icon');
   refs.darkIcon.classList.add('opacity-icon');
+
+  lightThemeStyles();
 }
