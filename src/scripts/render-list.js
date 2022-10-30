5e2e225e7afa52edf7434d2f7dcf0b86bbf7fff7
Serhii-Ter <serhij86@gmail.com>
2022-10-30 23:09:09 +0200

    Update render-list.js

--- src/scripts/render-list.js
+++ src/scripts/render-list.js
@@ -5,9 +5,15 @@ export function renderList(films) {
     .map(film => {
       const movieDate = new Date(film.release_date).getFullYear();
       const filmData = JSON.stringify(film);
-      return `<li class="card__item" data-filminfo='${filmData}' data-filmid="${film.id}">
+      return `<li class="card__item" data-filminfo='${filmData}' data-filmid="${
+        film.id
+      }">
         <div class="card__thumb">
-            <img class="card__img" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="${film.title}" onerror="this.onerror=null;this.src='https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-';">
+            <img class="card__img" src="https://image.tmdb.org/t/p/w500${
+              film.poster_path
+            }" alt="${
+        film.title
+      }" onerror="this.onerror=null;this.src='https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-';">
         </div>
         <h2 class="card__title">
         ${film.title}
@@ -15,11 +21,10 @@ export function renderList(films) {
         <p class="card__text">
             <span class="card__genres">${listOfGenres(
               Object.values(film.genre_ids)
-            )}</span> <span class="card__date">${movieDate}</span>
+            )}</span> | <span class="card__date">${movieDate}</span>
         </p>
         </li>`;
     })
     .join('');
-    console.log(markup)
   return markup;
 }
