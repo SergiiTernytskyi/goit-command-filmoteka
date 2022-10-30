b483fadf59ff8866b6a391fcbf8759a49c854a17
sonyaaat <sofiatkach2004@gmail.com>
2022-10-30 22:58:09 +0200

    Заглушки для фото які не підгрузилися

--- src/scripts/render-list.js
+++ src/scripts/render-list.js
@@ -5,17 +5,9 @@ export function renderList(films) {
     .map(film => {
 
       const filmData = JSON.stringify(film);
-      return `<li class="card__item" data-filminfo='${filmData}' data-filmid="${
-        film.id
-      }">
-
-     // const movieDate = new Date(film.release_date).getFullYear();
-   //   return `<li class="card__item" data-filmid="${film.id}">
-
+      return `<li class="card__item" data-filminfo='${filmData}' data-filmid="${film.id}">
         <div class="card__thumb">
-            <img class="card__img" src="https://image.tmdb.org/t/p/w500${
-              film.poster_path
-            }" alt="${film.title}">
+            <img class="card__img" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="${film.title}" onerror="this.onerror=null;this.src='https://ik.imagekit.io/tc8jxffbcvf/default-movie-portrait_EmJUj9Tda5wa.jpg?tr=fo-auto,di-';">
         </div>
         <h2 class="card__title">
         ${film.title}
@@ -23,10 +15,11 @@ export function renderList(films) {
         <p class="card__text">
             <span class="card__genres">${listOfGenres(
               Object.values(film.genre_ids)
-            )}</span> | <span class="card__date">${movieDate}</span>
+            )}</span> | <span class="card__date">${film.release_date}</span>
         </p>
         </li>`;
     })
     .join('');
+    console.log(markup)
   return markup;
 }
