308aa967d01836b2d72087ea948ebf700191bb36
Артем Гладуш <m1hommage5008@gmail.com>
2022-10-30 13:22:52 +0200

    bag-fix/apiService

--- src/js/apiServise.js
+++ src/js/apiServise.js
@@ -1,46 +1,52 @@
 import { API_KEY, TRENDING_URL, SEARCH_URL, ID_URL } from "./apiVars";
 import axios from 'axios';
 
-export default {
+export default class ApiService {
+constructor() {
+	this.page = 1;
+	this.searchQuery = ' ';
+	this.id = ' ';
+}
 
-	async getTrendData(page) {
-		try {
-			const {data} = await axios.get(
-				`${TRENDING_URL}?api_key=${API_KEY}&language=uk&page=${page}` 
-			)	
+async fetchTrendData() {
+	try {
+		const {data} = await axios.get(
+			`${TRENDING_URL}?api_key=${API_KEY}&language=uk&page=${this.page}` 
+		)	
+	return data;
+} catch (error) {
+	console.error('Smth wrong with api get full trends' + error);
+	}
+}
+
+async fetchMovieSearch() {
+	try {
+		const {data} = await axios.get(
+			`${SEARCH_URL}?api_key=${API_KEY}&language=uk&query=${this.searchQuery}&page=${this.page}`
+		);
+
 		return data;
 	} catch (error) {
-		console.error('Smth wrong with api get full trends' + error);
-		}
-	},
-
-	async fetchMovieSearch(text, page) {
-		try {
-			const {data} = await axios.get(
-				`${SEARCH_URL}?api_key=${API_KEY}&language=uk&query=${text}&page=${page}`
-			);
-
-			return data;
-		} catch (error) {
-			console.error( 'Smth wrong with api search fetch' + error);
-		}
-	},
-
-	async getMovieById(id) {
-		try {
-			const {data} = await axios.get(`${ID_URL}${id}?api_key=${API_KEY}&language=uk`);
-			return data;
-		} catch(error) {
-			console.error('Smth wrong with api ID fetch' + error)
-		}
-	},
-
-	async getMovieById(id) {
-		try {
-			const {data} = await axios.get(`${ID_URL}${id}/videosnpm run duild?api_key=${API_KEY}&language=uk`);
-			return data;
-		} catch(error) {
-			console.error('Smth wrong with api ID fetch' + error)
-		}
+		console.error( 'Smth wrong with api search fetch' + error);
 	}
+}
+
+async fetchMovieById() {
+	try {
+		const {data} = await axios.get(`${ID_URL}${this.id}?api_key=${API_KEY}&append_to_response=videos&language=uk`);
+		return data;
+	} catch(error) {
+		console.error('Smth wrong with api ID fetch' + error)
+	}
+}
+
+// async fetchTrailerById() {
+// 	try {
+// 		const {data} = await axios.get(`${ID_URL}${this.id}/videos?api_key=${API_KEY}&language=uk`);
+// 		return data;
+// 	} catch(error) {
+// 		console.error('Smth wrong with api ID fetch' + error)
+// 	}
+// }
+
 }
