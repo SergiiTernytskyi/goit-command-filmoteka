dde095a248b8b54beb7eb2672c705f6f574406e0
Viktor Annyenkov <annyenkov@gmail.com>
2022-10-31 21:49:00 +0200

    Update filter.js

--- src/scripts/filter.js
+++ src/scripts/filter.js
@@ -24,36 +24,43 @@ async function openListSort(e) {
     const response = await initFilter(genreUrl, sortUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   if (genre !== "start" && year !== "start" && sort === "start") {
     const response = await initFilter(genreUrl, yearUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   if (genre !== "start" && year === "start" && sort === "start") {
     const response = await initFilter(genreUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   if (genre === "start" && year !== "start" && sort === "start") {
     const response = await initFilter(yearUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   if (genre !== "start" && year !== "start" && sort !== "start") {
     const response = await initFilter(genreUrl, yearUrl, sortUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   if (genre === "start" && year !== "start" && sort !== "start") {
     const response = await initFilter(yearUrl, sortUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   if (genre === "start" && year === "start" && sort !== "start") {
     const response = await initFilter(sortUrl)
     const data = await response.data
     console.log(data)
+    return data
   }
   function initFilter(genreUrl, yearUrl, sortUrl) {
   return axios.get(`${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US${yearUrl}${genreUrl}${sortUrl}&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
