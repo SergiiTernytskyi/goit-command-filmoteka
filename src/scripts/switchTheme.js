9b61e1c782b8528a1c0776750b2e1e2b116f70e8
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-10-31 21:03:02 +0200

    update theme

--- src/scripts/switchTheme.js
+++ src/scripts/switchTheme.js
@@ -1,38 +1,40 @@
 const refs = {
+  body: document.querySelector('body'),
   darkBtn: document.querySelector('.them-dark-btn'),
   lightBtn: document.querySelector('.them-light-btn'),
   lightIcon: document.querySelector('.icon-light'),
   darkIcon: document.querySelector('.icon-dark'),
-  main: document.querySelector('main'),
-  footer: document.querySelector('footer'),
-  footerText: document.querySelectorAll('.footer-content'),
-  galleryTitle: document.getElementsByClassName('card__title'),
-  pagination: document.querySelector('.tui-pagination'),
 };
 
-function darkThemeStyles() {
-  refs.main.classList.add('main--dark');
-  refs.footer.classList.add('footer--dark');
-  refs.pagination.classList.add('pagination--dark');
-  refs.footerText.forEach(item => {
-    item.classList.add('footer-content--dark');
-  });
+// function darkThemeStyles() {
+//   refs.main.classList.add('main--dark');
+//   refs.footer.classList.add('footer--dark');
+//   refs.pagination.classList.add('pagination--dark');
+//   refs.footerText.forEach(item => {
+//     item.classList.add('footer-content--dark');
+//   });
 
-  for (let title of refs.galleryTitle) {
-    title.classList.add('card__title--dark');
-  }
-}
+//   for (let title of refs.galleryTitle) {
+//     title.classList.add('card__title--dark');
+//   }
+// }
 
-function lightThemeStyles() {
-  refs.main.classList.remove('main--dark');
-  refs.footer.classList.remove('footer--dark');
-  refs.pagination.classList.remove('pagination--dark');
-  refs.footerText.forEach(item => {
-    item.classList.remove('footer-content--dark');
-  });
-  for (let title of refs.galleryTitle) {
-    title.classList.remove('card__title--dark');
-  }
+// function lightThemeStyles() {
+//   refs.main.classList.remove('main--dark');
+//   refs.footer.classList.remove('footer--dark');
+//   refs.pagination.classList.remove('pagination--dark');
+//   refs.footerText.forEach(item => {
+//     item.classList.remove('footer-content--dark');
+//   });
+//   for (let title of refs.galleryTitle) {
+//     title.classList.remove('card__title--dark');
+//   }
+// }
+
+let savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  refs.body.classList = savedTheme;
+  console.log(savedTheme);
 }
 
 refs.darkBtn.addEventListener('click', onDarkBtnClick);
@@ -43,7 +45,10 @@ function onDarkBtnClick() {
   refs.lightIcon.classList.add('opacity-icon');
   refs.darkIcon.classList.remove('opacity-icon');
 
-  darkThemeStyles();
+  refs.body.classList.remove('light-theme');
+  refs.body.classList.add('dark-theme');
+
+  localStorage.setItem('theme', refs.body.classList);
 }
 
 refs.lightBtn.addEventListener('click', onLightBtnClick);
@@ -54,5 +59,13 @@ function onLightBtnClick() {
   refs.lightIcon.classList.remove('opacity-icon');
   refs.darkIcon.classList.add('opacity-icon');
 
-  lightThemeStyles();
+  refs.body.classList.remove('dark-theme');
+  refs.body.classList.add('light-theme');
+
+  localStorage.setItem('theme', refs.body.classList);
 }
+
+const theme = {
+  light: 'light-theme',
+  dark: 'dark-theme',
+};
