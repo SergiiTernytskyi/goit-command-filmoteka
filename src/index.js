cc345cf6b6c5cebb5295f6c3c5e807b26baa3d34
Serhii-Ter <serhij86@gmail.com>
2022-11-02 16:29:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 // import './library.js';
-import './js/fetchPopular';
 import 'animate.css/animate.css';
 import './scripts/render-list';
 import './scripts/genres-list';
 import './scripts/keyword/seachKeyword';
 import './scripts/trailer';
-import './js/fetchPopular';
 import './scripts/btnToTop';
 import './scripts/modalFilms';
 // import './js/modalFilm';
