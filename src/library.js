68240c078b2c7226f8996df4ae7decde969540e9
Serhii-Ter <serhij86@gmail.com>
2022-11-02 23:26:23 +0200

    pagination library

--- src/library.js
+++ src/library.js
@@ -2,8 +2,11 @@ import './scripts/modalFilms';
 import './scripts/team-modal';
 import './scripts/switchTheme';
 
+import { paginationHide } from './scripts/helpers/hide-pagination';
 import { loadWatchedFilms, setupLIbraryBtns } from './scripts/mylibrary';
 
 loadWatchedFilms();
 
 setupLIbraryBtns();
+
+// paginationHide();
